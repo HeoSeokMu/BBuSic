3e364c7061822b892535311cd3e0da0643df1b94
HeoSM <goldday3418@daum.net>
2014-11-18 16:53:27 +0900

    BBuMain System.out.println() 주석처리

--- BBuSic/src/BBu_main/BBuMain.java
+++ BBuSic/src/BBu_main/BBuMain.java
@@ -19,11 +19,13 @@ public class BBuMain implements Action, BBuSicAware{
 			myinfo_DTO = new payMyInfo_DTO();
 			System.out.println(sqlMapper);
 			myinfo_DTO = (payMyInfo_DTO)sqlMapper.queryForObject("payment_my.selectMyInfo", id);
+			/*
 			System.out.println("myinfoID : "+myinfo_DTO.getId());
 			System.out.println("myinfoCash : "+myinfo_DTO.getCash());
 			System.out.println("myinDownC : "+myinfo_DTO.getDownload_count());
 			System.out.println("myinfoPay_N : "+myinfo_DTO.getDelete_payname());
 			System.out.println("myinfoPay_B : "+myinfo_DTO.getDelete_paybenefit());
+			*/
 		}
 		
 		return SUCCESS;
