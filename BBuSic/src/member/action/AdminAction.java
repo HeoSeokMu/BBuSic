23a3988aa00435417a65e19341d36bc05771614a
KimJeongjin <wjdwlsdlfody@naver.com>
2014-11-20 23:03:31 +0900

    관리자페이지

---
+++ BBuSic/src/member/action/AdminAction.java
@@ -0,0 +1,11 @@
+package member.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class AdminAction implements Action{
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
