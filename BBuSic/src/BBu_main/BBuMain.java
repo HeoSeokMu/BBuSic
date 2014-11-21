3b2d64af9039071d9260b92dc3cbda4b67ed8ff9
Heohyosung <viento83@naver.com>
2014-11-21 09:26:27 +0900

    benefit 불러오는 컬럼 delete_paybenefit 으로 변경

--- BBuSic/src/BBu_main/BBuMain.java
+++ BBuSic/src/BBu_main/BBuMain.java
@@ -4,20 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import BBusic.Aware.musicAware;
-<<<<<<< HEAD
 
-=======
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
-<<<<<<< HEAD
-
-=======
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.opensymphony.xwork2.Action;
@@ -28,12 +21,6 @@ import payment.action.BBuSicAware;
 import payment.pay_setDTO.payMyInfo_DTO;
 import upload.dto.musicDTO;
 
-<<<<<<< HEAD
-	
-	
-=======
-
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 public class BBuMain implements Action, BBuSicAware, ServletRequestAware, ServletResponseAware, ModelDriven, Preparable,musicAware {
 	payMyInfo_DTO myinfo_DTO;
 	public static SqlMapClient sqlMapper;
@@ -41,10 +28,7 @@ public class BBuMain implements Action, BBuSicAware, ServletRequestAware, Servle
 	private String cooId = null;
 	private HttpServletResponse res;
 	private HttpServletRequest req;
-<<<<<<< HEAD
 	
-=======
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 	private List<musicDTO> musicPictureList = new ArrayList<musicDTO>();
 	private musicDTO mdto;
 	String file1;
@@ -54,11 +38,7 @@ public class BBuMain implements Action, BBuSicAware, ServletRequestAware, Servle
 	String file5;
 	String file6;
 	String file7;
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 	public String execute() throws Exception {
 		Cookie[] cookies = req.getCookies();
 		if(cookies != null){
@@ -108,11 +88,7 @@ public class BBuMain implements Action, BBuSicAware, ServletRequestAware, Servle
 	public payMyInfo_DTO getMyinfo_DTO() {
 		return myinfo_DTO;
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 	@Override
 	public Object getModel() {		
 		return mdto;
@@ -186,10 +162,7 @@ public class BBuMain implements Action, BBuSicAware, ServletRequestAware, Servle
 	public void setFile7(String file7) {
 		this.file7 = file7;
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 	
 	public void setServletResponse(HttpServletResponse res) {
 		this.res = res;
@@ -198,8 +171,4 @@ public class BBuMain implements Action, BBuSicAware, ServletRequestAware, Servle
 	public void setServletRequest(HttpServletRequest req) {
 		this.req = req;
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> 32c6b6b8fbc9a57852d36bc7649d1676ec803863
 }
