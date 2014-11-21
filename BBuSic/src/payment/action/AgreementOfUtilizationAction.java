ae71e8314a3a3e65f2a9f3491b63478a5a2b5d06
SeokMu <goldday3418@daum.net>
2014-11-21 14:07:06 +0900

    ggg

---
+++ BBuSic/src/payment/action/AgreementOfUtilizationAction.java
@@ -0,0 +1,10 @@
+package payment.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class AgreementOfUtilizationAction implements Action{
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
