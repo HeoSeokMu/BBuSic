af1a557d6833f5ddf7e43deeb22e89cc2a21aa7e
Ahn <alexgon421@naver.com>
2014-11-24 15:11:05 +0900

    최종2

---
+++ BBuSic/src/upload/action/UploadMainAction.java
@@ -0,0 +1,14 @@
+package upload.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class UploadMainAction implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+
+
+}
---
+++ BBuSic/src/upload/dto/likeDTO.java
@@ -0,0 +1,21 @@
+package upload.dto;
+
+public class likeDTO {
+	private int num;
+	private String id;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
+}
